b8835fc288653b55a3cc4cf23e0ebd0e9bdd0d94
Dikshit Bhardwaj <dikshit.b.122@gmail.com>
2023-05-14 10:14:59 +0530

    Class Implementation of Trie

--- Trie/ClassImpelmentationTrie.cpp
+++ Trie/ClassImpelmentationTrie.cpp
@@ -5,15 +5,89 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Node{
+class TrieNode{
 	public:
 		bool isEndString;
-		Node *child[26];
+		TrieNode *child[26];
+
+		// constructor
+		TrieNode(){
+			this->isEndString = false;
+			for(int i=0;i<26; i++)
+			{
+				this->child[i] = NULL;
+			}
+		}
+
+		void Insert(string word)
+		{
+			if(word.length()<=0) return;
+			TrieNode *temp = this;
+
+			for(int i=0; i<word.length(); i++)
+			{
+				int index = word[i] - 'a';
+				if(!temp->child[index]){
+					TrieNode *newNode = new TrieNode();
+					temp->child[index] = newNode;
+					cout<<word[i]<<" inserted successfully"<<endl;
+				}
+				temp = temp->child[index];
+			}
+		}
+
+		// search function
+		bool search(string word){
+			if(word.length()<=0) return false;
+			TrieNode *temp = this;
+			for(int i=0; i<word.length(); i++)
+			{
+				int index = word[i] - 'a';
+				if(!temp->child[index]){
+					return false;
+				}
+				temp = temp->child[index];
+			}
+			return true;
+		}
+
+		// search word with starting prefix
+		bool startWith(string word)
+		{
+			if(word.length()<=0) return false;
+			TrieNode *temp = this;
+			for(int i=0;i<word.length();i++)
+			{
+				int index = word[i] - 'a';
+				if(!temp->child[index] && i<word.length())
+				{
+					return false;
+				}
+				temp = temp->child[index];
+			}
+			return true;
+		}
 };
 
 
 
 int main()
 {
+	TrieNode *root = new TrieNode;
+	string word,key,key2;
+	cin>>word;
+	root->Insert(word);
+	cout<<"\nEnter prefix to search:";
+	cin>>key;
+	if(root->search(key))
+		cout<<key<<" is present;"<<endl;
+	else
+		cout<<key<<" is not present"<<endl;
+	cin>>key2;
+	if(root->startWith(key2))
+		cout<<key2<<" is present;"<<endl;
+	else
+		cout<<key2<<" is not present"<<endl;
+	
 	return 0;
 }
